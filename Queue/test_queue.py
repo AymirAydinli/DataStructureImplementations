be137bf06175f792d22a1299ae8170924da74ef1
Aymir <aaaxilles@gmail.com>
2021-09-26 14:52:20 +0200

    Queue Testing fixed

--- Queue/test_queue.py
+++ Queue/test_queue.py
@@ -9,21 +9,22 @@ class TestQueue(unittest.TestCase):
 
         self.assertEqual(queue.size(), 0, "first size should be 0")
 
-        queue.add(1)
-        queue.add(2)
-        queue.add(3)
-        queue.add(4)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        queue.enqueue(4)
 
         self.assertEqual(queue.size(), 4, "size should be 4")
         self.assertEqual(queue.peek(), 1, "peek should be 1")
-        self.assertEqual(queue.remove(), 1, "remove should be 1")
-        self.assertEqual(queue.remove(), 2, "remove should be 2")
+        self.assertEqual(queue.dequeue(), 1, "remove should be 1")
+        self.assertEqual(queue.dequeue(), 2, "remove should be 2")
         self.assertEqual(queue.size(), 2, "size should be 2")
         self.assertEqual(queue.peek(), 3, "peek should be 3")
-        self.assertEqual(queue.isEmpty(), None, "isEmpty should be False")
-        self.assertEqual(queue.remove(), 3, "remove should be 3")
-        self.assertEqual(queue.remove(), 4, "remove should be 4")
+        self.assertEqual(queue.isEmpty(), False, "isEmpty should be False")
+        self.assertEqual(queue.dequeue(), 3, "remove should be 3")
+        self.assertEqual(queue.dequeue(), 4, "remove should be 4")
         self.assertEqual(queue.isEmpty(), True, "isEmpty should be True")
+
 
 if __name__ == "__main__":
     unittest.main()
